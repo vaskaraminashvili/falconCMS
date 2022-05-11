323ad6cfe84b4541103a0cd1547b6a695dfd51f2
vaska <vaska.r02@gmail.com>
2022-05-11 17:51:16 +0400

    added-inertia

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::inertia('/', 'Welcome');
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome');
 });
